917423a763db16b08657decaab6c8e7ae19e1227
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-04-11 23:53:04 +0600

    plus-mins.js algorthms problem solve

--- plus-mins.js
+++ plus-mins.js
@@ -1,16 +1,12 @@
 function plusMinus(arr) {
-    /*
-     * Write your code here.
-     */
-    var n = parseInt(readLine());
-    arr = readLine().split(' ');
-    arr = arr.map(Number)
-    var positiveNumbers = arr.filter((number) => { if (number > 0) return Number(number) });
-    var negativeNumbers = arr.filter((number) => { if (number < 0) return Number(number) });
-    var zeros = arr.length - positiveNumbers.length - negativeNumbers.length;
+    // Complete this function
+    let positives = parseFloat(arr.filter(value => value > 0).length / arr.length).toPrecision(6) 
+    let negatives = parseFloat(arr.filter(value => value < 0).length / arr.length).toPrecision(6) 
+    let zeroes = parseFloat(arr.filter(value => value === 0).length / arr.length).toPrecision(6) 
+   
+    console.log(positives) 
+    console.log(negatives) 
+    console.log(zeroes) 
     
-    console.log(positiveNumbers.length / arr.length);
-    console.log(negativeNumbers.length / arr.length);
-    console.log(zeros / arr.length);
-
+    
 }
