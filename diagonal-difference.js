887d847c4c8ecffdae3147a4585cd04d35b48841
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-05-19 03:06:18 +0600

    diagonal-difference.js problem solve

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -1,12 +1,41 @@
-function diagonalDifference(a) {
-    /*
-     * Write your code here.
-     */
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
     var n = parseInt(readLine());
-    let sum = 0;
-    for (let i = 0; i < n; i++) {
-        sum += a[i][i] - a[i][n-1-i];
+    var a = [];
+    for(a_i = 0; a_i < n; a_i++){
+       a[a_i] = readLine().split(' ');
+       a[a_i] = a[a_i].map(Number);
     }
-    console.log(sum >= 0 ? sum : sum * -1);
+
+    var first_diagonal = 0;
+    var second_diagonal = 0;
+    for(i = 0; i < n; i++) {
+      first_diagonal += a[i][i];
+      second_diagonal += a[i][n - i - 1];
+    }
+
+    var result = Math.abs(first_diagonal - second_diagonal);
+    console.log(result);
 
 }
