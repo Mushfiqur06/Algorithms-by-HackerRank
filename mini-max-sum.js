24790cc3173e320412583de37b216ae4ca4371b7
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-06-25 14:16:30 +0600

    Remove mini-max-sm.js

--- mini-max-sum.js
+++
@@ -1,8 +0,0 @@
-var number = [1, 2, 3, 4, 5];
-
-var sum = number.reduce((acc, next) => acc + next);
-var min, max;
-min = sum - Math.max(...number);
-max = sum - Math.min(...number);
-
-console.log(min, max);
