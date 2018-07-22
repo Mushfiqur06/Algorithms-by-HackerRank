104ae606831fc9d141915be3653edc963604b2e8
mushfiqur06 <mushfiqur06@gmail.com>
2018-07-22 12:43:48 +0600

    StrongPass.js problem solve

--- Strings/strongPass.js
+++ Strings/strongPass.js
@@ -1,5 +1,4 @@
 function minimumNumber(n, password) {
-    // Return the minimum number of characters to make the password strong
     const c = [/[0-9]/, /[a-z]/, /[A-Z]/, /[!@#$%^&*()\-+"]/]
             .map(r => !r.test(password))
             .filter(Boolean).length
