d5a991450a3514dfcd52820ab69d76289752ccc1
mushfiqur06 <mushfiqur06@gmail.com>
2018-07-23 16:19:20 +0600

    StrongPass.js problem solve

--- Strings/strongPass.js
+++ Strings/strongPass.js
@@ -1,6 +1,5 @@
 function minimumNumber(n, password) {
-    const c = [/[0-9]/, /[a-z]/, /[A-Z]/, /[!@#$%^&*()\-+"]/]
-            .map(r => !r.test(password))
+    const c = [/[0-9]/, /[a-z]/, /[A-Z]/, /[!@#$%^&*()\-+"]/].map(r => !r.test(password))
             .filter(Boolean).length
     
     return Math.max(c,6-n);
